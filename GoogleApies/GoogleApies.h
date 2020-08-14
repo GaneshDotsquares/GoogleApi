1938d031257b6764021e843c90b38c1f3675d367
GaneshDotsquares <techdotsquares@gmail.com>
2020-08-14 12:02:32 +0530

    first commit

---
+++ GoogleApies/GoogleApies.h
@@ -0,0 +1,19 @@
+//
+//  GoogleApies.h
+//  GoogleApies
+//
+//  Created by Ganesh on 14/08/20.
+//  Copyright © 2020 Dotsquares. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoogleApies.
+FOUNDATION_EXPORT double GoogleApiesVersionNumber;
+
+//! Project version string for GoogleApies.
+FOUNDATION_EXPORT const unsigned char GoogleApiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleApies/PublicHeader.h>
+
+
---
+++ Pods/Target Support Files/Pods-GoogleApies/Pods-GoogleApies-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoogleApies : NSObject
+@end
+@implementation PodsDummy_Pods_GoogleApies
+@end
